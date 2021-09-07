dc545989c8d65a88010391c03c81b4f3b08faa32
BaianorASR <reis.adson2@outlook.com>
2021-09-07 11:28:17 -0300

    exercicio Date

---
+++ src/datetreino.java
@@ -0,0 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class datetreino {
+    public static void main(String[] args){
+
+        Date agora = new Date();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+
+        String dataFormatada = formatter.format(agora);
+
+        System.out.println(dataFormatada);
+
+    }
+}
